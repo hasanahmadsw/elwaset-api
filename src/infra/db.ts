0a3443146775130f0068a11d19e44eb7bcb84d96
Hasan Ahmad <hasanahmad.dev@gmail.com>
2022-09-28 00:24:08 +0400

    edit db config

--- src/infra/db.ts
+++ src/infra/db.ts
@@ -2,6 +2,7 @@ import {
   DATABASE_HOST,
   DATABASE_NAME,
   DATABASE_PASS,
+  DATABASE_PORT,
   DATABASE_USER,
 } from "../config";
 
@@ -10,16 +11,10 @@ import { JobCategory } from "../app/models/job-category.model";
 import { Job } from "../app/models/job.model";
 import { User } from "../app/models/user.model";
 
-export const db = new Sequelize(
-  DATABASE_NAME || "forsa",
-  DATABASE_USER || "root",
-  DATABASE_PASS || "root",
+export const db = new Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASS, {
+  host: DATABASE_HOST || "localhost",
+  port: +DATABASE_PORT,
 
-  {
-    host: DATABASE_HOST || "localhost",
-    port: +8889,
-
-    dialect: "mysql",
-    models: [JobCategory, Job, User],
-  }
-);
+  dialect: "mysql",
+  models: [JobCategory, Job, User],
+});
